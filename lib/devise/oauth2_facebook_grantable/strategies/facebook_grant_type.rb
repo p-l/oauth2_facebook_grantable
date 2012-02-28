dfd8c8c9d9d826ae06c02d1991399b54335103c6
Pierre-Luc Simard <plsimard@mirego.com>
2012-02-28 15:54:59 -0500

    Removed log messages

--- lib/devise/oauth2_facebook_grantable/strategies/facebook_grant_type.rb
+++ lib/devise/oauth2_facebook_grantable/strategies/facebook_grant_type.rb
@@ -6,7 +6,11 @@ module Devise
       end
 
       def authenticate_grant_type(client)
+        Rails.logger.debug("Searching for user: #{params[:facebook_identifier]}")
         resource = mapping.to.find_for_authentication(:facebook_identifier => params[:facebook_identifier])
+        if(!resource)
+          Rails.logger.debug("===> FACEBOOK USER WAS NOT FOUND")
+        end
         if validate(resource) { resource.valid_facebook_access_token?(params[:facebook_access_token]) }
           success! resource
         elsif !halted?
