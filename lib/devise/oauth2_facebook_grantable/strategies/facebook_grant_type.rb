b8d6d04ef02bce8b2abc40166f225bae4df2f59d
Pierre-Luc Simard <plsimard@mirego.com>
2012-02-27 19:30:03 -0500

    removed unused log lines

--- lib/devise/oauth2_facebook_grantable/strategies/facebook_grant_type.rb
+++ lib/devise/oauth2_facebook_grantable/strategies/facebook_grant_type.rb
@@ -7,12 +7,9 @@ module Devise
 
       def authenticate_grant_type(client)
         resource = mapping.to.find_for_authentication(:facebook_identifier => params[:facebook_identifier])
-        Rails.logger.debug("Looking for ressource... #{resource}")
         if validate(resource) { resource.valid_facebook_access_token?(params[:facebook_access_token]) }
-          Rails.logger.debug("Authenticated OK")
           success! resource
         elsif !halted?
-          Rails.logger.debug("*****FACEBOOK**** Authentication FAILED")
           oauth_error! :invalid_grant, 'could not authenticate to facebook'
         end
       end
